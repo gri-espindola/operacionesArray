4029761ca20550abc1a99a803ad6fb437b098436
gri-espindola <griespindola@hotmail.com>
2021-10-20 21:28:47 -0300

    guardando maquetado operaciones-ejercicio

---
+++ ejercicios-grupal.js
@@ -0,0 +1,46 @@
+const operaciones = [
+    {
+      descripcion: 'Sueldo',
+      categoria: 'Trabajo',
+      fecha: '01/01/2021',
+      monto: 50000,
+      tipo: 'Ganancia',
+    },
+    {
+      descripcion: 'Pago de alquiler',
+      categoria: 'Alquiler',
+      fecha: '02/01/2021',
+      monto: 15000,
+      tipo: 'Gasto',
+    },
+    {
+      descripcion: 'Pago de expensas',
+      categoria: 'Alquiler',
+      fecha: '02/01/2021',
+      monto: 5000,
+      tipo: 'Gasto',
+    },
+    {
+      descripcion: 'Pago de internet',
+      categoria: 'Servicios',
+      fecha: '10/01/2021',
+      monto: 2000,
+      tipo: 'Gasto',
+    },
+    {
+      descripcion: 'Trabajo freelance',
+      categoria: 'Trabajo',
+      fecha: '15/01/2021',
+      monto: 20000,
+      tipo: 'Ganancia',
+    },
+    {
+      descripcion: 'Cena con amigas',
+      categoria: 'Salidas',
+      fecha: '18/01/2021',
+      monto: 1500,
+      tipo: 'Gasto',
+    },
+  ];
+
+  
